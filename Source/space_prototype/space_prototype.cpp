86426b44eb07375a0b12d22474fe69a04095befa
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-05-16 21:32:54 +0200

    Initial commit

---
+++ Source/space_prototype/space_prototype.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "space_prototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, space_prototype, "space_prototype" );
